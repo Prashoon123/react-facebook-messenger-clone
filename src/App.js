1b3e4203d2819c6a3712e8e7ba1dcab82e5ca2b8
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-14 13:23:04 +0530

    Clean up your REACT app DONE✅

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    // BEM naming convention
+    <div className="app">
+      <h1>Yooooo, let's build the Facebook Messenger Clone🚀</h1>
+    </div>
+  );
+}
+
+export default App;
