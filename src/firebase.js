8aa168960408826e3a424bb4b04d592d781c6db6
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-10-19 09:28:19 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB4g_tptwCC819EN64ha5fxxfU691dRHU0",
-    authDomain: "messenger-clone-abc.firebaseapp.com",
-    databaseURL: "https://messenger-clone-abc.firebaseio.com",
-    projectId: "messenger-clone-abc",
-    storageBucket: "messenger-clone-abc.appspot.com",
-    messagingSenderId: "262601146388",
-    appId: "1:262601146388:web:641e295a61b2d49912e701",
-    measurementId: "G-0BSN7MFQQ1",
+    // ENTER YOUR APP'S FIREBASE CONFIG HERE
 });
 
 const db = firebaseApp.firestore();
